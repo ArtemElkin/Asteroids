548977d85e21a7ca0ce32abffd7557e08859cf46
ArtemElkin <elkin.artem@protonmail.com>
2026-07-16 20:02:44 +0300

    refactor: add structs for spaceship parts to more accurate Create() in SpaceshipFactory
    fix:
    5. SpaceshipFactory. Стало лучше, но сейчас слишком много out параметров. Методы не возвращают понятный результат, а просто вываливают наружу пачку переменных. Лучше не таскать их через out, а сгруппировать в небольшие структуры, например, ViewParts, PhysicsParts, HealthParts. Тогда основной Create будет читаться как сборка из готовых частей и станет чище.

--- Assets/_Project/Infrastructure/Factories/SpaceshipFactory.cs
+++ Assets/_Project/Infrastructure/Factories/SpaceshipFactory.cs
@@ -28,65 +28,85 @@ namespace _Project.Infrastructure.Factories
 
         public override SpaceshipFacade Create(SpaceshipSpawnData data)
         {
-            CreateView(data.initialMovementData.initialPosition,
-                out var view, 
-                out var drawable);
-            CreatePhysicsComponents(data.initialMovementData, data.config.movementConfig, view,
-                out var movementModel, 
-                out var movable, 
-                out var rotatable,
-                out var collidable,
-                out var boundsChecker);
-            CreateWeapons(view, movementModel, data.config, 
-                out var weapons);
-            CreateHealth(data.config, out var healthController, 
-                out var deathHandler);
-            CreateClones(data.hasClones, movementModel, boundsChecker, drawable, 
-                out var screenWrapCloneSet);
-            CreateStunController(view, movementModel, data.config, collidable, screenWrapCloneSet, 
-                out var stunController);
+            var viewParts = CreateView(data.initialMovementData.initialPosition);
+            var physicsParts = CreatePhysicsComponents(data.initialMovementData, 
+                data.config.movementConfig, viewParts.View);
+            var weapons = CreateWeapons(viewParts.View, 
+                physicsParts.Model, data.config);
+            var healthParts = CreateHealth(data.config);
+            var screenWrapCloneSet = CreateClones(data.hasClones, physicsParts.Model, 
+                physicsParts.BoundsChecker, viewParts.Drawable);
+            var stunController = CreateStunController(viewParts.View, physicsParts.Model, data.config, 
+                physicsParts.Collidable, screenWrapCloneSet);
 
-            CreateSpaceshipFacade(movementModel, movable, rotatable, boundsChecker, drawable, collidable,
-                healthController, deathHandler, stunController, weapons, screenWrapCloneSet,
-                out var facade);
+            var facade = CreateSpaceshipFacade(viewParts, physicsParts, 
+                healthParts, stunController, weapons, screenWrapCloneSet);
             
             return facade;
         }
-
-        private void CreateView(Vector2 initialPosition, out SpaceshipView view, out IDrawable drawable)
+        
+        private struct ViewParts
         {
-            view = _pool.Get();
-            drawable = view;
-            drawable.Setup(initialPosition, 0);
+            public SpaceshipView View { get; }
+            public IDrawable Drawable { get; }
+
+            public ViewParts(SpaceshipView view, IDrawable drawable)
+            {
+                View = view;
+                Drawable = drawable;
+            }
         }
 
-        private void CreatePhysicsComponents(
+        private ViewParts CreateView(Vector2 initialPosition)
+        {
+            var view = _pool.Get();
+            var drawable = view;
+            drawable.Setup(initialPosition, 0);
+            
+            return new ViewParts(view, drawable);
+        }
+
+        private struct PhysicsParts
+        {
+            public MovementModel Model { get; }
+            public IMovable Movable { get; }
+            public IRotatable Rotatable { get; }
+            public ICollidable Collidable { get; }
+            public BoundsChecker BoundsChecker { get; }
+
+            public PhysicsParts(MovementModel model, IMovable movable, IRotatable rotatable, 
+                ICollidable collidable, BoundsChecker boundsChecker)
+            {
+                Model = model;
+                Movable = movable;
+                Rotatable = rotatable;
+                Collidable = collidable;
+                BoundsChecker = boundsChecker;
+            }
+        }
+
+        private PhysicsParts CreatePhysicsComponents(
             InitialMovementData initialMovementData,
             SpaceshipMovementConfig movementConfig,
-            SpaceshipView view,
-            out MovementModel movementModel, 
-            out IMovable movable,
-            out IRotatable rotatable,
-            out ICollidable collidable,
-            out BoundsChecker boundsChecker
-            )
+            SpaceshipView view)
         {
-            movementModel = CreateComponent<MovementModel>(initialMovementData);
-            collidable = view.GetComponent<ICollidable>();
+            var movementModel = CreateComponent<MovementModel>(initialMovementData);
+            var collidable = view.GetComponent<ICollidable>();
             collidable.Setup(movementModel, true);
             
-            movable = CreateComponent<SpaceshipMovementController>(movementModel, movementConfig);
-            rotatable = CreateComponent<SpaceshipRotationController>(movementModel);
-            boundsChecker = CreateComponent<BoundsChecker>(movementModel);
+            var movable = CreateComponent<SpaceshipMovementController>(movementModel, movementConfig);
+            var rotatable = CreateComponent<SpaceshipRotationController>(movementModel);
+            var boundsChecker = CreateComponent<BoundsChecker>(movementModel);
+            
+            return new PhysicsParts(movementModel,  movable, rotatable, collidable, boundsChecker);
         }
 
-        private void CreateWeapons(
+        private Dictionary<WeaponType, IWeapon> CreateWeapons(
             SpaceshipView view, 
             MovementModel movementModel, 
-            SpaceshipConfig config, 
-            out Dictionary<WeaponType, IWeapon> weapons)
+            SpaceshipConfig config)
         {
-            weapons = new Dictionary<WeaponType, IWeapon>();
+            var weapons = new Dictionary<WeaponType, IWeapon>();
             
             MuzzleView muzzleView = view.GetMuzzleView();
             IHasPosition muzzlePosition = muzzleView;
@@ -97,36 +117,36 @@ namespace _Project.Infrastructure.Factories
             LaserWeapon laserWeapon =
                 CreateComponent<LaserWeapon>(config.laserWeaponConfig, movementModel, muzzlePosition);
             weapons.Add(WeaponType.LaserWeapon, laserWeapon);
+            
+            return weapons;
         }
 
-        private void CreateStunController(
+        private StunController CreateStunController(
             SpaceshipView view, 
             MovementModel movementModel, 
             SpaceshipConfig config, 
             ICollidable collidable, 
-            IScreenWrapCloneSet screenWrapCloneSet,
-            out StunController stunController)
+            IScreenWrapCloneSet screenWrapCloneSet)
         {
             IEffect originStunEffect = view.GetComponentInChildren<IEffect>();
             IEffect syncedStunEffect = CreateComponent<SyncedSpaceshipStunEffect>(
                 originStunEffect, 
                 screenWrapCloneSet);
             
-            stunController = CreateComponent<StunController>(
+            return CreateComponent<StunController>(
                 config.stunDuration,
                 movementModel, 
                 collidable, 
                 syncedStunEffect);
         }
 
-        private void CreateClones(
+        private IScreenWrapCloneSet CreateClones(
             bool hasClones, 
             MovementModel movementModel, 
             BoundsChecker boundsChecker, 
-            IDrawable drawable,
-            out IScreenWrapCloneSet screenWrapCloneSet)
+            IDrawable drawable)
         {
-            screenWrapCloneSet = hasClones
+            IScreenWrapCloneSet screenWrapCloneSet = hasClones
                 ? CreateComponent<ScreenWrapCloneSet<SpaceshipFacade>>(
                     movementModel,
                     boundsChecker,
@@ -134,42 +154,49 @@ namespace _Project.Infrastructure.Factories
                 : new NullScreenWrapCloneSet();
             
             screenWrapCloneSet.CreateClones();
+            
+            return screenWrapCloneSet;
         }
 
-        private void CreateHealth(
-            SpaceshipConfig config, 
-            out HealthController healthController, 
-            out SpaceshipDeathHandler deathHandler)
+        private struct HealthParts
+        {
+            public HealthController HealthController { get; }
+            public SpaceshipDeathHandler DeathHandler { get; }
+
+            public HealthParts(HealthController healthController, SpaceshipDeathHandler deathHandler)
+            {
+                HealthController = healthController;
+                DeathHandler = deathHandler;
+            }
+        }
+
+        private HealthParts CreateHealth(SpaceshipConfig config)
         {
             var healthModel = CreateComponent<HealthModel>(config.healthConfig.maxHealth); 
             
-            healthController = CreateComponent<HealthController>(healthModel);
-            deathHandler = CreateComponent<SpaceshipDeathHandler>();
+            var healthController = CreateComponent<HealthController>(healthModel);
+            var deathHandler = CreateComponent<SpaceshipDeathHandler>();
+            
+            return new HealthParts(healthController, deathHandler);
         }
 
-        private void CreateSpaceshipFacade(
-            MovementModel movementModel,
-            IMovable movable,
-            IRotatable rotatable,
-            BoundsChecker boundsChecker,
-            IDrawable drawable,
-            ICollidable collidable,
-            HealthController healthController,
-            SpaceshipDeathHandler deathHandler,
+        private SpaceshipFacade CreateSpaceshipFacade(
+            ViewParts viewParts,
+            PhysicsParts physicsParts,
+            HealthParts healthParts,
             StunController stunController,
             Dictionary<WeaponType, IWeapon> weapons,
-            IScreenWrapCloneSet screenWrapCloneSet,
-            out SpaceshipFacade facade)
+            IScreenWrapCloneSet screenWrapCloneSet)
         {
-            facade = CreateComponent<SpaceshipFacade>(
-                movementModel,
-                movable,
-                rotatable,
-                boundsChecker,
-                drawable,
-                healthController,
-                deathHandler,
-                collidable,
+            return CreateComponent<SpaceshipFacade>(
+                physicsParts.Model,
+                physicsParts.Movable,
+                physicsParts.Rotatable,
+                physicsParts.Collidable,
+                physicsParts.BoundsChecker,
+                viewParts.Drawable,
+                healthParts.HealthController,
+                healthParts.DeathHandler,
                 stunController,
                 weapons,
                 screenWrapCloneSet);
